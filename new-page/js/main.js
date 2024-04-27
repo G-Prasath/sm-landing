57eeb41262c9be71b3b41a3169049cbbf441edc1
G-Prasath <guruprasathmsc@gmail.com>
2024-04-27 13:11:32 +0530

    Initial Layout Commit

---
+++ new-page/js/main.js
@@ -0,0 +1,121 @@
+
+(function($) {
+    "use strict";
+
+    $(document).on('ready', function() {
+     
+
+        /* ==================================================
+            # Smooth Scroll
+         ===============================================*/
+        $("body").scrollspy({
+            target: ".navbar-collapse",
+            offset: 200
+        });
+        $('a.smooth-menu').on('click', function(event) {
+            var $anchor = $(this);
+            var headerH = '75';
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - headerH + "px"
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+
+
+        /* ==================================================
+            # Equal Height Init
+        ===============================================*/
+        $(window).on('resize', function() {
+            $(".equal-height").equalHeights();
+        });
+
+        $(".equal-height").equalHeights().find("img, iframe, object").on('load', function() {
+            $(".equal-height").equalHeights();
+        });
+
+
+        /* ==================================================
+            # Fun Factor Init
+        ===============================================*/
+        $('.timer').countTo();
+        $('.fun-fact').appear(function() {
+            $('.timer').countTo();
+        }, {
+            accY: -100
+        });
+
+
+        /* ==================================================
+            # Our Work Flow Carousel
+         ===============================================*/
+        $('.our-offer-carousel').owlCarousel({
+            loop: false,
+            margin: 30,
+            nav: false,
+            navText: [
+                "<i class='fa fa-angle-left'></i>",
+                "<i class='fa fa-angle-right'></i>"
+            ],
+            dots: true,
+            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
+            }
+        });
+
+
+        /* ==================================================
+            Preloader Init
+         ===============================================*/
+        $(window).on('load', function() {
+            // Animate loader off screen
+            $(".se-pre-con").fadeOut("slow");;
+        });
+
+
+        /* ==================================================
+            Contact Form Validations
+        ================================================== */
+        $('.contact-form').each(function() {
+            var formInstance = $(this);
+            formInstance.submit(function() {
+
+                var action = $(this).attr('action');
+
+                $("#message").slideUp(750, function() {
+                    $('#message').hide();
+
+                    $('#submit')
+                        .after('<img src="assets/img/ajax-loader.gif" class="loader" />')
+                        .attr('disabled', 'disabled');
+
+                    $.post(action, {
+                            name: $('#name').val(),
+                            email: $('#email').val(),
+                            phone: $('#phone').val(),
+                            comments: $('#comments').val()
+                        },
+                        function(data) {
+                            document.getElementById('message').innerHTML = data;
+                            $('#message').slideDown('slow');
+                            $('.contact-form img.loader').fadeOut('slow', function() {
+                                $(this).remove()
+                            });
+                            $('#submit').removeAttr('disabled');
+                        }
+                    );
+                });
+                return false;
+            });
+        });
+
+    }); // end document ready function
+})(jQuery); // End jQuery
